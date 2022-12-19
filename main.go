07d1d1e2988f313d8e299f064abfbfa161b6fde3
Ravi Pandey <ravip@Ravis-Mac-mini.local>
2022-12-19 23:42:38 +0530

    added APIs to create, run, stop/remove container to be used to run tasks

--- main.go
+++ main.go
@@ -2,68 +2,112 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
-	"github.com/codding-buddha/mini-kube/manager"
-	"github.com/codding-buddha/mini-kube/node"
 	"github.com/codding-buddha/mini-kube/task"
-	"github.com/codding-buddha/mini-kube/worker"
-	"github.com/golang-collections/collections/queue"
-	"github.com/google/uuid"
+	"github.com/docker/docker/client"
 )
 
 func main() {
-	t := task.Task{
-		ID:     uuid.New(),
-		Name:   "Task-1",
-		State:  task.Pending,
-		Image:  "Image-1",
-		Memory: 1024,
-		Disk:   1,
+	// t := task.Task{
+	// 	ID:     uuid.New(),
+	// 	Name:   "Task-1",
+	// 	State:  task.Pending,
+	// 	Image:  "Image-1",
+	// 	Memory: 1024,
+	// 	Disk:   1,
+	// }
+
+	// te := task.TaskEvent{
+	// 	ID:        uuid.New(),
+	// 	State:     task.Pending,
+	// 	Timestamp: time.Now(),
+	// 	Task:      t,
+	// }
+
+	// fmt.Printf("task: %v\n", t)
+	// fmt.Printf("task event: %v\n", te)
+
+	// w := worker.Worker{
+	// 	Name:  "worker1",
+	// 	Queue: *queue.New(),
+	// 	Db:    make(map[uuid.UUID]task.Task),
+	// }
+
+	// fmt.Printf("worker: %v\n", w)
+	// w.CollectStats()
+	// w.RunTasks()
+	// w.StartTask()
+	// w.StopTask()
+
+	// m := manager.Manager{
+	// 	Pending: *queue.New(),
+	// 	TaskDb:  make(map[string][]task.Task),
+	// 	EventDb: make(map[string][]task.TaskEvent),
+	// 	Workers: []string{w.Name},
+	// }
+
+	// fmt.Printf("manager: %v\n", m)
+	// m.SelectWorker()
+	// m.UpdateTasks()
+	// m.SendWork()
+
+	// n := node.Node{
+	// 	Name:   "Node-1",
+	// 	Ip:     "192.168.1.1",
+	// 	Cores:  4,
+	// 	Memory: 1024,
+	// 	Disk:   25,
+	// 	Role:   "worker",
+	// }
+
+	// fmt.Printf("node: %v\n", n)
+	fmt.Printf("Create test container\n")
+	dockerTask, createResult := createContainer()
+	if createResult.Error != nil {
+		fmt.Printf("%v\n", createResult.Error)
+		os.Exit(1)
 	}
 
-	te := task.TaskEvent{
-		ID:        uuid.New(),
-		State:     task.Pending,
-		Timestamp: time.Now(),
-		Task:      t,
+	time.Sleep(time.Second * 5)
+	fmt.Printf("stopping container %s\n", createResult.ContainerId)
+	_ = stopContainer(dockerTask)
+}
+
+func createContainer() (*task.Docker, *task.DockerResult) {
+	c := task.Config{
+		Name:  "test-container-1",
+		Image: "postgres:13",
+		Env: []string{
+			"POSTGRES_USER=cube",
+			"POSTGRES_PASSWORD=secret",
+		},
 	}
 
-	fmt.Printf("task: %v\n", t)
-	fmt.Printf("task event: %v\n", te)
-
-	w := worker.Worker{
-		Name:  "worker1",
-		Queue: *queue.New(),
-		Db:    make(map[uuid.UUID]task.Task),
+	dc, _ := client.NewClientWithOpts(client.FromEnv)
+	d := task.Docker{
+		Config: c,
+		Client: dc,
 	}
 
-	fmt.Printf("worker: %v\n", w)
-	w.CollectStats()
-	w.RunTasks()
-	w.StartTask()
-	w.StopTask()
-
-	m := manager.Manager{
-		Pending: *queue.New(),
-		TaskDb:  make(map[string][]task.Task),
-		EventDb: make(map[string][]task.TaskEvent),
-		Workers: []string{w.Name},
+	result := d.Run()
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil, nil
 	}
 
-	fmt.Printf("manager: %v\n", m)
-	m.SelectWorker()
-	m.UpdateTasks()
-	m.SendWork()
+	fmt.Printf("Container %s is running with config %v\n", result.ContainerId, c)
+	return &d, &result
+}
 
-	n := node.Node{
-		Name:   "Node-1",
-		Ip:     "192.168.1.1",
-		Cores:  4,
-		Memory: 1024,
-		Disk:   25,
-		Role:   "worker",
+func stopContainer(d *task.Docker) *task.DockerResult {
+	result := d.Stop()
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil
 	}
 
-	fmt.Printf("node: %v\n", n)
+	fmt.Printf("Container %s has been stopped and removed\n", result.ContainerId)
+	return &result
 }
--- task/task.go
+++ task/task.go
@@ -1,8 +1,16 @@
 package task
 
 import (
+	"context"
+	"io"
+	"log"
+	"os"
 	"time"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/docker/go-connections/nat"
 	"github.com/google/uuid"
 )
@@ -36,4 +44,120 @@ type TaskEvent struct {
 	State     State
 	Timestamp time.Time
 	Task      Task
+}
+
+type Config struct {
+	Name          string
+	AttachStdin   bool
+	AttachStdout  bool
+	AttachStderr  bool
+	Cmd           []string
+	Image         string
+	Memory        int64
+	Disk          int64
+	Env           []string
+	RestartPolicy string
+}
+
+type Docker struct {
+	Client      *client.Client
+	Config      Config
+	ContainerId string
+}
+
+type DockerResult struct {
+	Error       error
+	Action      string
+	ContainerId string
+	Result      string
+}
+
+func (docker *Docker) Run() DockerResult {
+	ctx := context.Background()
+	reader, err := docker.Client.ImagePull(ctx, docker.Config.Image, types.ImagePullOptions{})
+
+	if err != nil {
+		log.Printf("Error pulling image %v: %v\n", docker.Config.Image, err)
+	}
+
+	io.Copy(os.Stdout, reader)
+
+	rp := container.RestartPolicy{
+		Name: docker.Config.RestartPolicy,
+	}
+
+	r := container.Resources{
+		Memory: docker.Config.Memory,
+	}
+
+	cc := container.Config{
+		Image: docker.Config.Image,
+		Env:   docker.Config.Env,
+	}
+
+	hc := container.HostConfig{
+		RestartPolicy:   rp,
+		Resources:       r,
+		PublishAllPorts: true,
+	}
+
+	resp, err := docker.Client.ContainerCreate(
+		ctx,
+		&cc,
+		&hc,
+		nil,
+		nil,
+		docker.Config.Name,
+	)
+
+	if err != nil {
+		log.Printf("Error creating container using image %s:%v\n", docker.Config.Image, err)
+		return DockerResult{Error: err}
+	}
+
+	err = docker.Client.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{})
+
+	if err != nil {
+		log.Printf("Error starting container %s:%v\n", resp.ID, err)
+		return DockerResult{Error: err}
+	}
+
+	out, err := docker.Client.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true})
+
+	if err != nil {
+		log.Printf("Error getting logs for container %s: %v\n", resp.ID, err)
+		return DockerResult{Error: err}
+	}
+
+	docker.ContainerId = resp.ID
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+
+	return DockerResult{
+		ContainerId: resp.ID,
+		Action:      "start",
+		Result:      "success",
+	}
+}
+
+func (docker *Docker) Stop() DockerResult {
+	ctx := context.Background()
+	log.Printf("Attempting to stop container %v", docker.ContainerId)
+	err := docker.Client.ContainerStop(ctx, docker.ContainerId, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	removeOptions := types.ContainerRemoveOptions{
+		RemoveVolumes: true,
+		RemoveLinks:   false,
+		Force:         false,
+	}
+
+	err = docker.Client.ContainerRemove(ctx, docker.ContainerId, removeOptions)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return DockerResult{Action: "stop", Result: "success", Error: nil}
 }
